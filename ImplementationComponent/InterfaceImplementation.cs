a289cb50e46571da94fbf00e1dcaa9a66a3592ab
jasonxz <jasonxz@outlook.com>
2021-11-11 18:44:43 -0600

    Code
    Actually upload the code....(eyeroll)

---
+++ ImplementationComponent/InterfaceImplementation.cs
@@ -0,0 +1,16 @@
+﻿using InterfaceDefinitionComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationComponent {
+
+	public sealed class InterfaceImplementation : InterfaceDefinition {
+
+		public int GetInt() {
+			return 1;
+		}
+	}
+}
